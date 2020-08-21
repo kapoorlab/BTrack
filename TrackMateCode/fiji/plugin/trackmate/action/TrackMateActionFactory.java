d67f71dc4a8f6e2c532e003f0334814ac82a8b1f
kapoorlab <randomaccessiblekapoor@gmail.com>
2020-08-21 11:50:53 +0200

    remove trackmate

--- TrackMateCode/fiji/plugin/trackmate/action/TrackMateActionFactory.java
+++
@@ -1,9 +0,0 @@
-package fiji.plugin.trackmate.action;
-
-import fiji.plugin.trackmate.TrackMateModule;
-import fiji.plugin.trackmate.gui.TrackMateGUIController;
-
-public interface TrackMateActionFactory extends TrackMateModule
-{
-	public TrackMateAction create( TrackMateGUIController controller );
-}
