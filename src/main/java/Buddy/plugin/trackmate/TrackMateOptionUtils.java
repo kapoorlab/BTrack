82fc7feae0f5be39d1223236db90b2a7fd380a32
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-01-18 02:50:47 +0100

    breaking changes

--- src/main/java/Buddy/plugin/trackmate/TrackMateOptionUtils.java
+++ src/main/java/Buddy/plugin/trackmate/TrackMateOptionUtils.java
@@ -6,11 +6,12 @@ import org.scijava.options.OptionsService;
 import ij.IJ;
 
 public class TrackMateOptionUtils {
-	private TrackMateOptionUtils() {
-	}
+	private TrackMateOptionUtils()
+	{}
 
-	public static TrackMateOptions getOptions() {
-		final Context ctx = (Context) IJ.runPlugIn("org.scijava.Context", "");
-		return ctx.getService(OptionsService.class).getOptions(TrackMateOptions.class);
+	public static TrackMateOptions getOptions()
+	{
+		final Context ctx = ( Context ) IJ.runPlugIn( "org.scijava.Context", "" );
+		return ctx.getService( OptionsService.class ).getOptions( TrackMateOptions.class );
 	}
 }
